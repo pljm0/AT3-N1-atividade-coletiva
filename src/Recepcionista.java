2b1436886ceaa8fe9acb7ccacd80020c1e633351
Pedro Lourenço <pedrolourencojm@gmail.com>
2024-05-05 00:13:27 -0300

    update classe Recepcionista

--- src/Recepcionista.java
+++ src/Recepcionista.java
@@ -2,8 +2,27 @@ class Recepcionista extends Thread {
     private String nome;
     private Hotel hotel;
 
+    public Recepcionista() {
+
+    }
     public Recepcionista(String nome, Hotel hotel) {
         this.nome = nome;
+        this.hotel = hotel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
         this.hotel = hotel;
     }
 
